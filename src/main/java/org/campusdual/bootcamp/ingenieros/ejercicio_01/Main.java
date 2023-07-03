989390dbba8ee89710bcdefb768292225b1a043f
Alba Vazquez <runo-mola12@hotmail.es>
2023-07-04 00:02:18 +0200

    Commit

--- src/main/java/org/campusdual/bootcamp/ingenieros/ejercicio_01/Main.java
+++ src/main/java/org/campusdual/bootcamp/ingenieros/ejercicio_01/Main.java
@@ -1,19 +1,10 @@
 package org.campusdual.bootcamp.ingenieros.ejercicio_01;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int numero = Input.integer("Escriba un numero entero: ");
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        if(numero%2 == 0) System.out.println("El numero es par");
+        else System.out.println("El numero es impar");
     }
 }
