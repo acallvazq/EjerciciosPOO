b3d15a0c9e3d70ae86df9b1f8eea4621a48e8b8d
Alba Vazquez <runo-mola12@hotmail.es>
2023-07-04 17:17:55 +0200

    Revisado ejercicio 10

--- src/main/java/org/campusdual/bootcamp/ingenieros/ejercicio_10/main/Main.java
+++ src/main/java/org/campusdual/bootcamp/ingenieros/ejercicio_10/main/Main.java
@@ -1,6 +1,7 @@
-package org.campusdual.bootcamp.ingenieros.ejercicio_09.main;
+package org.campusdual.bootcamp.ingenieros.ejercicio_10.main;
 
-import org.campusdual.bootcamp.ingenieros.ejercicio_09.classes.Animal;
+import org.campusdual.bootcamp.ingenieros.ejercicio_10.classes.Animal;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -11,10 +12,22 @@ public class Main {
         Animal animal2 = new Animal("Ave","Ave rapaz", "Elanio azul", "Aereo");
         Animal animal3 = new Animal("Mamifero","Gato", "American shorthair", "Terrestre");
 
+        //Constructor de copia
+        Animal animal4 = new Animal(animal3);
+
+        //Metodo clone
+        Animal animal5 = null;
+        try {
+            animal5 = animal3.clone();
+        }catch(CloneNotSupportedException cnse){
+            cnse.printStackTrace();
+        }
+
         //Mostrar por pantalla los datos
         System.out.println("Animal 1: " + animal1);
         System.out.println("Animal 2: " + animal2);
         System.out.println("Animal 3: " + animal3);
-
+        System.out.println("Animal 4: " + animal4);
+        System.out.println("Animal 5: " + animal5);
     }
 }
