3d90828b65c0227b282adea23b7e290c0ebf7a23
Alba Vazquez <runo-mola12@hotmail.es>
2023-07-04 12:22:39 +0200

    Ejercicio 9 hecho

---
+++ src/main/java/org/campusdual/bootcamp/ingenieros/ejercicio_09/Animal.java
@@ -0,0 +1,65 @@
+package org.campusdual.bootcamp.ingenieros.ejercicio_09;
+
+public class Animal {
+    //Atributos
+    private String reino;
+    private String tipo;
+    private String raza;
+    private String medio;
+
+    //Constructores
+    public Animal(){
+
+    }
+
+    public Animal(String reino, String tipo, String raza, String medio){
+        this.reino = reino;
+        this.tipo = tipo;
+        this.raza = raza;
+        this.medio = medio;
+    }
+
+    //Metodos
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "reino='" + reino + '\'' +
+                ", tipo='" + tipo + '\'' +
+                ", raza='" + raza + '\'' +
+                ", medio='" + medio + '\'' +
+                '}';
+    }
+
+    //Getters y Setters
+    public String getReino() {
+        return reino;
+    }
+
+    public void setReino(String reino) {
+        this.reino = reino;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public String getMedio() {
+        return medio;
+    }
+
+    public void setMedio(String medio) {
+        this.medio = medio;
+    }
+}
