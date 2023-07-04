7e483d14961d5a8c439d68937cb2efd449693a4d
Alba Vazquez <runo-mola12@hotmail.es>
2023-07-04 13:22:19 +0200

    Ejercicio 11 hecho

---
+++ src/main/java/org/campusdual/bootcamp/ingenieros/ejercicio_11/AnimaldeGranja.java
@@ -0,0 +1,34 @@
+package org.campusdual.bootcamp.ingenieros.ejercicio_11;
+
+public class AnimaldeGranja extends Animal implements Apropiable{
+    //Atributos
+    private String propietario;
+
+    //Constructores
+    public AnimaldeGranja(){
+
+    }
+
+    public AnimaldeGranja(String reino, String tipo, String raza, String medio, String nombre, String propietario) {
+        super(reino, tipo, raza, medio);
+        this.propietario = propietario;
+    }
+
+    //Metodos
+
+    @Override
+    public String toString() {
+        return "AnimaldeGranja{" +
+                "propietario='" + propietario + '\'' +
+                '}';
+    }
+
+    //Getters y Setters
+    public String getPropietario(){
+        return propietario;
+    }
+    public void setPropietario(String propietario){
+        this.propietario = propietario;
+    }
+
+}
---
+++ src/main/java/org/campusdual/bootcamp/ingenieros/ejercicio_11/Apropiable.java
@@ -0,0 +1,6 @@
+package org.campusdual.bootcamp.ingenieros.ejercicio_11;
+
+public interface Apropiable{
+    String getPropietario();
+    void setPropietario(String propietario);
+}
